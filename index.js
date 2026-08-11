0dcd26643556e7861d459fb19727104fcf06f605
Mario <curkovicmario9.7@gmail.com>
2026-08-11 15:04:54 +0200

    Add the first working version of the issue, and the test file

--- index.js
+++ index.js
@@ -1,8 +1,88 @@
-import hashColor from 'hash-color-material';
+import ansis, { bold, cyan, hex, red, rgb } from 'ansis';
+// import ansis color library for terminals
+import randomColor from 'randomcolor';
 
-console.log(hashColor.getColorFromString('Hello world!'));
+// import an npm called "randomcolor" for generating random colors
 
-hashColor.getColorFromString('Hello world!');
+const color = randomColor();
+const colorHueLuminosity= randomColor({
+  luminosity: process.argv[2],
+  hue: process.argv[3]
+});
+const colorHue = randomColor({
+  hue: process.argv[2]
+});
+const colorLuminosity = randomColor({
+  luminosity: process.argv[2]
+});
 
-const color = hashColor[Math.floor(Math.random()* hashColor.length)];
-console.log(color);
+/*console.log(console.log(hex(colorHueLuminosity)(`
+###############################
+###############################
+###############################
+#####                     #####
+#####       ${colorHueLuminosity}       #####
+#####                     #####
+###############################
+###############################
+###############################
+`)));
+*/
+
+
+if (process.argv[2] === 'red' || 'blue' || 'green' ){
+  console.log(hex(colorHue)(`
+###############################
+###############################
+###############################
+#####                     #####
+#####       ${colorHue}       #####
+#####                     #####
+###############################
+###############################
+###############################
+`));
+}else if(process.argv[2] === 'light' || 'dark'){
+  console.log(console.log(hex(colorLuminosity)(`
+###############################
+###############################
+###############################
+#####                     #####
+#####       ${colorLuminosity}       #####
+#####                     #####
+###############################
+###############################
+###############################
+`)));
+}else if((process.argv[2]=== 'red' || 'blue' || 'green' ) && (process.argv[3] === 'light' || 'dark')){
+  console.log(hex(colorHueLuminosity)(`
+###############################
+###############################
+###############################
+#####                     #####
+#####       ${colorHueLuminosity}       #####
+#####                     #####
+###############################
+###############################
+###############################
+`));
+
+  }else {
+  console.log(hex(color)(`
+###############################
+###############################
+###############################
+#####                     #####
+#####       ${color}       #####
+#####                     #####
+###############################
+###############################
+###############################
+`));
+};
+
+
+
+
+//console.log(process.argv[2]);
+//console.log(randColor);
